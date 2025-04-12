3f8af09e4f41507abc62024ffd279c993ecc8612
Damssi13 <eddamssirirachid@gmail.com>
2025-04-12 03:47:07 +0100

    No need to make the derived class destructor virtual

--- module-04/ex00/Cat.hpp
+++ module-04/ex00/Cat.hpp
@@ -8,7 +8,7 @@ public:
     Cat();
     Cat(const Cat &other);
     Cat  &operator=(const Cat &other);
-    virtual ~Cat();
+    ~Cat();
 
     void    makeSound() const;
 };
--- module-04/ex00/main.cpp
+++ module-04/ex00/main.cpp
@@ -5,14 +5,28 @@
 
 int main()
 {
+    std::cout << "\n===============================\n";
+    std::cout << "\n=====Constructing======\n";
 
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
+    std::cout << "\n===============================\n";
+    std::cout << "\n======GettingType=======\n";
+
     std::cout << i->getType() << " " << std::endl;
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << meta->getType() << " " << std::endl;
+    std::cout << "\n===============================\n";
+    std::cout << "\n=======MakeSound=======\n";
+
     i->makeSound(); //will output the cat sound!
     j->makeSound();
     meta->makeSound();
+    std::cout << "\n===============================\n";
+    std::cout << "\n=======Destruction=======\n";
+    delete i;
+    delete j;
+    delete meta;
     return 0;
 }
